2dfb402667d167fd1b5d9c0aae580aa475267f99
YixSoviet <69943781+Yix-Soviet@users.noreply.github.com>
2026-09-05 10:33:20 -0500

    Init: Primer commit, estructura base del proyecto

---
+++ src/main/java/mdw_proyectofinal/vetlife/VetlifeApplication.java
@@ -0,0 +1,13 @@
+package mdw_proyectofinal.vetlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mdw_proyectofinal/vetlife/VetlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package mdw_proyectofinal.vetlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
