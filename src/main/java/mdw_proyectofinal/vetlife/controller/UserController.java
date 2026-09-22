edd9bd16aa5b36c009189ee1af267f1c779c7689
Dayana Sandoval <dayana.sandovaln@gmail.com>
2026-09-22 11:24:59 -0500

    Agrega endpoints y vistas de perfil y mascota

--- src/main/java/mdw_proyectofinal/vetlife/controller/UserController.java
+++ src/main/java/mdw_proyectofinal/vetlife/controller/UserController.java
@@ -1,5 +1,21 @@
 package mdw_proyectofinal.vetlife.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
-    
+
+    @GetMapping("/perfil")
+    public String verPerfil(Model model) {
+        model.addAttribute("nombreUsuario", "Grisel");
+        return "perfil";
+    }
+
+    @GetMapping("/mascota")
+    public String verMascota(Model model) {
+        model.addAttribute("nombreMascota", "Fido");
+        return "mascota";
+    }
 }
